a250c89b98c8f583f85ef158eddc7035abccf262
OgunladeFavour <favourogunlade69@gmail.com>
2022-06-30 02:09:27 -0700

    my commit for permissions

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -12,5 +13,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-© 2022 GitHub, Inc.
-Footer navigation
